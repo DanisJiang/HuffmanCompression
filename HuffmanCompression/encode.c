59fe87b5e256db8ff9425c387c23f5becb471860
danisjiang <danisjiang@gmail.com>
2019-05-27 12:14:11 +0800

    inputFile

--- HuffmanCompression/encode.c
+++ HuffmanCompression/encode.c
@@ -1,15 +1,37 @@
 ﻿#include<stdio.h>
+#include<string.h>
 #include"encode.h"
 
 void encode(char *argv[])
 {
-	FILE* inputFp = open(argv[2]);
+	FILE* inputFp;
+	errno_t err;
+	if (err = fopen_s(&inputFp, argv[2],"r"))
+	{
+		printf("error: no such file");
+		exit(0);
+	}
+	else
+	{
+		printf("success to open %s\n\n", argv[2]);
+	}
 	char s[1000];
-	fscanf_s(inputFp,"%s",s);
+	if (!readFile(inputFp, s))
+	{
+		printf("error: failed to read file");
+		exit(0);
+	}
 	printf("%s",s);
 	//int *freq = frequency();
-	
-	printf("This is encode func\n");
+}
+
+int readFile(FILE *fp, char* s)
+{
+	int i = 0;
+	while (fgets(fp, 1000, s+i))
+	{
+		i += strlen(s);
+	}
 }
 
 int* frequency(char* c)
--- HuffmanCompression/encode.h
+++ HuffmanCompression/encode.h
@@ -2,3 +2,5 @@
 void encode(char *argv[]);
 
 int* frequency(char* c);
+
+int readFile(FILE *fp, char* s);
